4d4f219db19f4abd14e881ad238e465f905008b7
Anderson <anderson.hung@live.com>
2019-01-24 11:48:47 -0500

    add points of interest to map

--- src/DataMap.js
+++ src/DataMap.js
@@ -1,28 +1,45 @@
 import React, { Component } from "react";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Map, TileLayer, Marker } from "react-leaflet";
+
+import "./DataMap.css";
 
 class DataMap extends Component {
 	state = {
+		pois: [],
 		lat: 51.505,
 		lng: -0.09,
 		zoom: 13
 	};
 	render() {
-		const position = [this.state.lat, this.state.lng];
-
+		const { pois } = this.state;
+		if (!pois.length) {
+			return null;
+		}
 		return (
-			<Map center={position} zoom={this.state.zoom}>
+			<Map center={[pois[0].lat, pois[0].lon]} zoom={this.state.zoom}>
 				<TileLayer
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 				/>
-				<Marker position={position}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
+				{this.renderPois()}
 			</Map>
 		);
+	}
+
+	renderPois() {
+		return this.state.pois.map(point => (
+			<Marker key={point.poi_id} position={[point.lat, point.lon]} />
+		));
+	}
+
+	componentDidMount() {
+		this.fetchPois();
+	}
+
+	async fetchPois() {
+		const response = await fetch("poi");
+		const pois = await response.json();
+		this.setState({ pois });
 	}
 }
 
